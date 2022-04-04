f5300f18af8ea8d174a55a77c5f552a6d54a935a
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-04-05 02:17:54 +0530

    [FEAT]: created the wishlist page card component

---
+++ src/components/Wishlist/WishlistPageCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function WishlistPageCard() {
+  return (
+    <div className="shadow-2xl rounded flex flex-col relative w-72 mb-12 ml-12 bg-rose-200">
+      <div className="h-72">
+        <img
+          className="img-responsive rounded"
+          style={{ height: "100%" }}
+          src="https://picsum.photos/200"
+          alt="dress"
+        />
+
+        <div className="px-4 py-2 text-xl text-white text-badge card-badge bg-slate-900">
+          new
+        </div>
+      </div>
+
+      <div className="p-3">
+        <h3 className="text-grey-600 subtitle1 mb-2">Printed floral dress</h3>
+        <h4 className="subtitle2">Girls white printed dress</h4>
+        <h5 className="text-grey-600 mb-4">sold by marge</h5>
+        <p className="body1 mb-3">
+          White Flower print fit & flare dress. Square neck Short, puff sleeves.
+          Knee length in flared hem Crepe fabric with concealed zip closure.
+        </p>
+        <div className="flex justify-around">
+          <button className="btn btn-square-solid btn-xs bg-slate-900  text-rose-200 mr-3 font-bold text-2xl">
+            <span className="fas fa-shopping-cart"></span>add to cart
+          </button>
+          <button className="btn btn-square-solid btn-xs bg-slate-900 text-rose-200 text-2xl">
+            <span className="fa-solid fa-trash-can"></span>remove from wishlist
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WishlistPageCard;
