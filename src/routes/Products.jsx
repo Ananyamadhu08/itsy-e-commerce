9d323911727030c9f1e4828e52bd5014fd50b5a3
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-04-05 00:26:12 +0530

    [FEAT]: added product page

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import FilterSidebar from "../components/Product-page/FilterSidebar";
+import SortProductsDropdown from "../components/Product-page/SortProductsDropdown";
+import ProductPageCard from "../components/Product-page/ProductPageCard";
 
 function Products() {
-  return <div>Products</div>;
+  return (
+    <main
+      className="relative w-full justify-end flex"
+      style={{ top: "5rem", minHeight: "100vh" }}
+    >
+      <FilterSidebar />
+
+      <div className="product__display__container">
+        <SortProductsDropdown />
+
+        <div
+          className="grid grid-cols-3  mr-20"
+          style={{ gap: "4rem", marginTop: "-2rem" }}
+        >
+          <ProductPageCard />
+          <ProductPageCard />
+          <ProductPageCard />
+          <ProductPageCard />
+          <ProductPageCard />
+          <ProductPageCard />
+        </div>
+
+        <div className="spacer-3rem"></div>
+        <div className="spacer-3rem"></div>
+        <div className="spacer-3rem"></div>
+        <div className="spacer-3rem"></div>
+      </div>
+    </main>
+  );
 }
 
 export default Products;
