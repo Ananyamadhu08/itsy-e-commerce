de26e7a84b7ce59c8aebf7e05e9c912e0d399f59
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-04-05 05:46:38 +0530

    [FEAT]: created login page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import LoginPage from "../components/auth-pages/LoginPage";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="relative" style={{ bottom: "2rem" }}>
+      <LoginPage />
+      <div className="spacer-3rem"></div>
+    </div>
+  );
 }
 
 export default Login;
