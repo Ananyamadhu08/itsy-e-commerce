d9eaa51e3fa7de135e57ca08d778bf65e5295b88
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-04-05 05:47:19 +0530

    [FEAT]: created sign up page

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import SignupPage from "../components/auth-pages/SignupPage";
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="relative" style={{ bottom: "2rem" }}>
+      <SignupPage />
+      <div className="spacer-3rem"></div>
+    </div>
+  );
 }
 
 export default Signup;
