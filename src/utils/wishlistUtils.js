f6b8a8a53bc3854bdb3b218d21e47918551a1260
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-04-16 17:59:39 +0530

    [FEAT]: added add product to wishlist functionality

--- src/utils/wishlistUtils.js
+++ src/utils/wishlistUtils.js
@@ -21,3 +21,33 @@ export const getWishlistItems = async (encodedToken, wishlistDispatch) => {
     wishlistDispatch({ type: wishlistActions.ERROR, payload: error });
   }
 };
+
+export const addProductToWishilist = async (
+  encodedToken,
+  product,
+  wishlistDispatch
+) => {
+  try {
+    wishlistDispatch({ type: wishlistActions.LOADING });
+
+    const {
+      data: { wishlist },
+      status,
+    } = await axios.post(
+      "api/user/wishlist",
+      {
+        headers: { authorization: encodedToken },
+      },
+      { product }
+    );
+
+    if (status >= 200 && status < 300) {
+      wishlistDispatch({
+        type: wishlistActions.GET_ALL_WISHLIST,
+        payload: wishlist,
+      });
+    }
+  } catch (error) {
+    wishlistDispatch({ type: wishlistActions.ERROR, payload: error });
+  }
+};
